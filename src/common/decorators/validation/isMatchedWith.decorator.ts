3f9ee1a7e9af9e6339e7a10375201a4dc1abd8f0
Mohamed El-Deeb <dev.mohamed20@gmail.com>
2026-02-11 14:45:40 +0200

    Clean up commented debug information in validator
    Removed commented-out debug information from the validate method.

--- src/common/decorators/validation/isMatchedWith.decorator.ts
+++ src/common/decorators/validation/isMatchedWith.decorator.ts
@@ -9,27 +9,6 @@ import {
 @ValidatorConstraint({ name: 'isMatchedWithConstrain', async: false })
 class IsMatchedWithConstrain implements ValidatorConstraintInterface {
   validate(value: string, args: ValidationArguments): boolean {
-    /**
-     * {
-  args: {
-    targetName: 'SignupDTO',
-    property: 'confirmPassword',
-    object: SignupDTO {
-      fullName: 'mohamed mahmoud',
-      username: 'ZSvber',
-      email: 'zsvber@gmail.com',
-      phone: '01288873997',
-      password: 'Mohamed@123',
-      confirmPassword: 'Mohamed@123',
-      birthDate: undefined
-    },
-    value: 'Mohamed@123',
-    constraints: [ 'password' ]
-  },
-  value: 'Mohamed@123'
-}
-     * */
-
     return args.object[args.constraints[0] as string] == value;
   }
   defaultMessage(args: ValidationArguments): string {
